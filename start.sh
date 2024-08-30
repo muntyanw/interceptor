cc9fc3bc929ef31c246e7d0c8c15c13525c3a4cc
ValeryM <muntyanw@gmail.com>
2024-08-30 21:37:16 +0300

    36

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /usr/share/nginx/html/interceptor
 source venv/bin/activate
- uvicorn telegram_interceptor.asgi:application --host 0.0.0.0 --port 8000
+uvicorn telegram_interceptor.asgi:application --host 0.0.0.0 --port 8000
