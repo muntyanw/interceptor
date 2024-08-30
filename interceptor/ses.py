06a79b296250c64cb3beb2bbe9959920dd26a578
ValeryM <muntyanw@gmail.com>
2024-08-30 15:46:19 +0300

    27

--- interceptor/ses.py
+++ interceptor/ses.py
@@ -7,7 +7,7 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from  routing import websocket_urlpatterns
+from routing import websocket_urlpatterns
 
 
 api_id = 24364263
