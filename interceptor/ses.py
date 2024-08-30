1720b5cc1501eb22fe98bb87cc6c3d15f1fbd363
ValeryM <muntyanw@gmail.com>
2024-08-30 15:48:49 +0300

    29

--- interceptor/ses.py
+++ interceptor/ses.py
@@ -7,7 +7,6 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from routing import websocket_urlpatterns
 
 
 api_id = 24364263
@@ -23,16 +22,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'telegram_interceptor.settings')
 # Инициализация Django перед использованием любых моделей или компонентов
 logger.info("[asgi] django.setup()")
 django.setup()
-
-logger.info("[asgi] interceptor.routing.websocket_urlpatterns = {URLRouter(interceptor.routing.websocket_urlpatterns)}")
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
-})
 
 # Настройки подключения к базе данных из settings.py
 db_config = {
