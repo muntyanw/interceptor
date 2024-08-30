6b83fd8c44da71cef40beeaaa45e9e01c1e71ff2
ValeryM <muntyanw@gmail.com>
2024-08-30 15:43:04 +0300

    26

--- interceptor/ses.py
+++ interceptor/ses.py
@@ -7,7 +7,7 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import routing
+from  routing import websocket_urlpatterns
 
 
 api_id = 24364263
@@ -29,7 +29,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            routing.websocket_urlpatterns
+            websocket_urlpatterns
         )
     ),
 })
